a8e348e39e9742918dad6c3932c4c0769fa91219
LordBaryhobal <47377497+LordBaryhobal@users.noreply.github.com>
2021-04-16 23:18:45 +0200

    Update telegram_bot.php

--- files/v1.0/telegram_bot.php
+++ files/v1.0/telegram_bot.php
@@ -1,4 +1,22 @@
 <?php
+/*** COPYRIGHT NOTICE *********************************************************
+    ProjeQtOr Telegram Bot
+    Copyright (C) 2021  HERDAC - Louis HEREDERO - baryhobal@herdac.ch
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    
+*** DO NOT REMOVE THIS NOTICE ************************************************/
 
 function log_msg($msg) {
 	error_log("Telegram Bot: ".$msg);
